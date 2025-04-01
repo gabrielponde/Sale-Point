7df2277e6d63b798329ed716fff77b086d9a0cc7
Gabriel Pondé <gabrielpavena@gmail.com>
2025-03-31 21:28:44 -0300

    Ajuste no backend

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -38,11 +38,7 @@ console.log('Registering entities:', entities.map(e => e.name));
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: port,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${port}/${process.env.DB_NAME}`,
   
   // Entity configuration
   entities: entities,
